3e56b72e816e74504a6087bd925d63c2a88ecec2
Kevin Rodrigues <kevrodrigues116@gmail.com>
2015-06-01 10:30:38 +0100

    Added resize method.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ var BUBBLE = {
 	//set up some values..
 	width: 320,
 	height: 480,
-	//
+	
+	//populate values later..
 	ratio: null,
 	currentWidth: null,
 	currentHeight: null,
@@ -26,5 +27,15 @@ var BUBBLE = {
 		//setting this so the browser doesn't default to 320..
 		this.canvas.width = this.width;
 		this.canvas.height = this.height;
-	} 
+
+		//get the canvas context "2d" so we
+		//can interact with it's api..
+		this.ctx = this.canvas.getContext('2d');
+
+		//call resize method..
+		this.resize();
+	},
+	resize: function() {
+
+	}
 };
